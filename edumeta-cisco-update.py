23ba9552c91466eaa4afe2d5837405eddd0fd752
Jonas Andersson <ja@kth.se>
2013-03-03 12:11:20 +0100

    minor bug fixes

--- edumeta-cisco-update.py
+++ edumeta-cisco-update.py
@@ -12,10 +12,17 @@ except ImportError:
 	sys.exit()
 # sudo pip install requests
 # sudo pip install pysnmp	
-				
+			
+def snmpget(host,oid,cmty='public'):
+	errorIndication, errorStatus, errorIndex,varBinds = cmdgen.CommandGenerator().getCmd(cmdgen.CommunityData(host, cmty),cmdgen.UdpTransportTarget((host, 161)),(oid))
+	return varBinds[0][1]
+
+def snmpwalk(host,oid,cmty='public'):
+	errorIndication, errorStatus, errorIndex,varBinds = cmdgen.CommandGenerator().nextCmd(cmdgen.CommunityData(host, cmty),cmdgen.UdpTransportTarget((host, 161)),(oid))
+	return [x[0] for x in varBinds]
+
 def pollwlc(host,cmty='public'):
-	errorIndication, errorStatus, errorIndex,varBinds = cmdgen.CommandGenerator().nextCmd(cmdgen.CommunityData(host, cmty),cmdgen.UdpTransportTarget((host, 161)),('1.3.6.1.4.1.14179.2.2.1.1.3'))
-	return [str(x[0][1]) for x in varBinds]
+	return [str(x[1]) for x in snmpwalk(host,'1.3.6.1.4.1.14179.2.2.1.1.3',cmty)]
 
 def getapcount(wlcs,sep='-'):
 	aps=sum([pollwlc(wlc) for wlc in wlcs],[])
@@ -53,11 +60,13 @@ def parseopt(args):
 if __name__ == '__main__':
 	(url,api_key,username,wlcs)=parseopt(sys.argv)
 	c=getapcount(wlcs)
-	print "%s ap:s" % sum(zip(*c.items())[1])
+	print "%s ap:s" % sum([v for k,v in c.items()])
 	r=get("%s?limit=0" % url,headers={'content-type': 'application/json','x-edumeta-username': username,'x-edumeta-api-key':api_key})
 	if r.status_code!=codes.ok: sys.exit()
 	for x in json.loads(r.text)['objects']:
-		if x['ap_no']!=c[x['location_shortname']]:
+		location_ap_no=c.get(x['location_shortname'],0)
+		#if location_ap_no==0 it should probably be removed (at least after some time)
+		if x['ap_no']!=location_ap_no:
 			patch("%s%s/" % (url,x['id']),headers={'content-type': 'application/json','x-edumeta-username': username,'x-edumeta-api-key': api_key},data=json.dumps({'ap_no':c[x['location_shortname']]}))
 			print "%s patched" % x['location_shortname']
 		else:
